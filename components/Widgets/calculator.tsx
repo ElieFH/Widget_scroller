48898b460b27447291f978420dd5bfbfbb6b24c0
Elie <elie.fourreau-hardy@epitech.eu>
2025-11-03 16:18:07 +0100

    [FEAT] Calc Widget: added more buttons, a 'previous operation' line and stronger checks on button presses to avoid incorect operation

--- components/Widgets/calculator.tsx
+++ components/Widgets/calculator.tsx
@@ -5,27 +5,62 @@ const keyboardValues = [
   "7", "8", "9", "*",
   "4", "5", "6", "-",
   "1", "2", "3", "+",
-  "C", "="
+  "0", ".", "=", "/",
+  "C"
 ];
+
+const isSymbol = (value: String): Boolean => {
+  if (value === '+' ||
+      value === '*' ||
+      value === '-' ||
+      value === "/"
+    ) {
+    return true
+  }
+  return false
+}
 
 export function Calculator({style} : ViewProps) {
   const [calcString, setCalcString] = useState("");
+  const [currentNumber, setCurrentNumber] = useState("");
+  const [prevString, setPrevString] = useState("");
+
+  //console.log("calcString: " + calcString + "\n" + "currentNumber: " + currentNumber);
   
   const calcButton = (buttonValue: string) => {
     return (
       <TouchableOpacity
         onPress={() => {
+          if (buttonValue === "C") {
+            setCalcString("")
+            setCurrentNumber("")
+            setPrevString("")
+            return
+          }
+          if (currentNumber === "0" && buttonValue !== "." && !isSymbol(buttonValue) ||
+              buttonValue === "." && (currentNumber === "" || currentNumber.includes(".")) ||
+              calcString === "NaN" || calcString === "Infinity" || calcString === "-Infinity" ||
+              isSymbol(buttonValue) && (calcString === "" || isSymbol(calcString.slice(-1))))
+            return
           if (buttonValue === "=") {
             try {
-              setCalcString(eval(calcString))
+              if (!isSymbol(calcString!.slice(-1)))
+                setPrevString(calcString + "=")
+                setCurrentNumber("")
+                setCalcString(eval(calcString).toString())
             } catch (e) {
+              setCurrentNumber("")
               setCalcString("")
             }
           } else {
-            buttonValue === "C" ? setCalcString("") : setCalcString(calcString + buttonValue)
+            if (isSymbol(buttonValue))
+              setCurrentNumber("");
+            else
+              setCurrentNumber(currentNumber + buttonValue);
+            setCalcString(calcString + buttonValue)
           }
         }}
-        style={styles.button}
+        style={[styles.button, {backgroundColor: buttonValue === "=" ? "#96beeb" : isSymbol(buttonValue) ? "#cde5ee" : "#dbe9ec"}]}
         key={buttonValue}
       >
         <Text>{buttonValue}</Text>
@@ -39,6 +74,7 @@ export function Calculator({style} : ViewProps) {
     >
       <Text style={styles.titleText}>Calculator:</Text>
       <View style={styles.calcInput}>
+        <Text style={styles.prevText}>{prevString}</Text>
         <Text style={styles.calcText}>{calcString}</Text>
       </View>
       <View style={styles.calcButtonsArea}>
@@ -57,8 +93,15 @@ const styles = StyleSheet.create({
   titleText: {
     fontSize: 20,
   },
+  prevText: {
+    fontSize: 20,
+    color: "#a3a3a3ff",
+    alignSelf: 'flex-end',
+    paddingHorizontal: "5%",
+  },
   calcText: {
     fontSize: 25,
+    alignSelf: 'flex-end',
     paddingHorizontal: "5%",
   },
   calcInput: {
@@ -67,7 +110,7 @@ const styles = StyleSheet.create({
     borderColor: "gray",
     borderWidth: 1,
     alignSelf: 'center',
-    flexDirection: 'row-reverse',
+    flexDirection: 'column',
     marginTop: "5%",
     paddingVertical: 5,
     width: "80%",
